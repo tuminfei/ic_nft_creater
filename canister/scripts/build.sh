eea4089d15e074959d673ed382f222c98a00a82a
Terry.Tu <tuminfei1981@gmail.com>
2023-11-22 09:40:56 +0800

    add deploy

--- canister/scripts/build.sh
+++ canister/scripts/build.sh
@@ -26,3 +26,16 @@ dfx canister call icrc7 icrc7_mint '(record{
   subaccount=null;
   };
 })'
+
+dfx deploy icrc7_with_assets --argument '(record {
+  tx_window=24;
+  permitted_drift=2;
+  name="Space";
+  symbol="Space";
+  minting_authority=opt principal"3yyxm-t5fpe-v32em-ac6lr-xyort-wuscb-dvl4x-3wnwi-hqkyj-xortw-oqe";
+  royalties=null;
+  royalties_recipient=null;    
+  description=opt "ICRC7 Standard Token";
+  image=null;    
+  supply_cap=null;    
+})'
