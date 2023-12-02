45f09811a617742d5fad279bdb2948e71ed8e9b9
Terry.Tu <tuminfei1981@gmail.com>
2023-12-02 17:50:49 +0800

    add nft collection

---
+++ prisma/migrations/20231202095013_add_nft_collection/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "NFTCollection" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "owner" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "tx_window" INTEGER NOT NULL,
+    "permitted_drift" INTEGER NOT NULL,
+    "royalties" INTEGER NOT NULL,
+    "royalties_recipient" TEXT NOT NULL,
+    "supply_cap" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
