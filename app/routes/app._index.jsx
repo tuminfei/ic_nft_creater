02327b064b046c40bb9d64793eb1712aff4726e7
terry.tu <tuminfei1981@gmail.com>
2023-12-18 08:55:20 +0800

    add app index

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,126 +1,59 @@
 import { json } from "@remix-run/node";
 import { useLoaderData, Link, useNavigate } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
+import React from "react";
 import {
   Card,
   EmptyState,
   Layout,
   Page,
-  IndexTable,
-  Thumbnail,
+  Grid,
+  LegacyCard,
+  MediaCard,
   Text,
-  Icon,
+  BlockStack,
   InlineStack,
 } from "@shopify/polaris";
-
-import { getQRCodes } from "../models/QRCode.server";
-import { DiamondAlertMajor, ImageMajor } from "@shopify/polaris-icons";
-
-export async function loader({ request }) {
-  const { admin, session } = await authenticate.admin(request);
-  const qrCodes = await getQRCodes(session.shop, admin.graphql);
-
-  return json({
-    qrCodes,
-  });
-}
-
-const EmptyQRCodeState = ({ onAction }) => (
-  <EmptyState
-    heading="Create unique QR codes for your product"
-    action={{
-      content: "Create QR code",
-      onAction,
-    }}
-    image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-  >
-    <p>Allow customers to scan codes and buy products using their phones.</p>
-  </EmptyState>
-);
-
-function truncate(str, { length = 25 } = {}) {
-  if (!str) return "";
-  if (str.length <= length) return str;
-  return str.slice(0, length) + "…";
-}
-
-const QRTable = ({ qrCodes }) => (
-  <IndexTable
-    resourceName={{
-      singular: "QR code",
-      plural: "QR codes",
-    }}
-    itemCount={qrCodes.length}
-    headings={[
-      { title: "Thumbnail", hidden: true },
-      { title: "Title" },
-      { title: "Product" },
-      { title: "Date created" },
-      { title: "Scans" },
-    ]}
-    selectable={false}
-  >
-    {qrCodes.map((qrCode) => (
-      <QRTableRow key={qrCode.id} qrCode={qrCode} />
-    ))}
-  </IndexTable>
-);
-
-const QRTableRow = ({ qrCode }) => (
-  <IndexTable.Row id={qrCode.id} position={qrCode.id}>
-    <IndexTable.Cell>
-      <Thumbnail
-        source={qrCode.productImage || ImageMajor}
-        alt={qrCode.productTitle}
-        size="small"
-      />
-    </IndexTable.Cell>
-    <IndexTable.Cell>
-      <Link to={`qrcodes/${qrCode.id}`}>{truncate(qrCode.title)}</Link>
-    </IndexTable.Cell>
-    <IndexTable.Cell>
-      {qrCode.productDeleted ? (
-        <InlineStack align="start" gap="200">
-          <span style={{ width: "20px" }}>
-            <Icon source={DiamondAlertMajor} tone="critical" />
-          </span>
-          <Text tone="critical" as="span">
-            product has been deleted
-          </Text>
-        </InlineStack>
-      ) : (
-        truncate(qrCode.productTitle)
-      )}
-    </IndexTable.Cell>
-    <IndexTable.Cell>
-      {new Date(qrCode.createdAt).toDateString()}
-    </IndexTable.Cell>
-    <IndexTable.Cell>{qrCode.scans}</IndexTable.Cell>
-  </IndexTable.Row>
-);
+import logo from "../images/logo.png";
 
 export default function Index() {
-  const { qrCodes } = useLoaderData();
-  const navigate = useNavigate();
-
   return (
-    <Page>
-      <ui-title-bar title="QR codes">
-        <button variant="primary" onClick={() => navigate("/app/qrcodes/new")}>
-          Create QR code
-        </button>
-      </ui-title-bar>
-      <Layout>
-        <Layout.Section>
-          <Card padding="0">
-            {qrCodes.length === 0 ? (
-              <EmptyQRCodeState onAction={() => navigate("qrcodes/new")} />
-            ) : (
-              <QRTable qrCodes={qrCodes} />
-            )}
-          </Card>
-        </Layout.Section>
-      </Layout>
+    <Page fullWidth>
+      <BlockStack gap="500">
+        <MediaCard
+          title="Getting Started"
+          primaryAction={{
+            content: "Learn about getting started",
+            onAction: () => {},
+          }}
+          description="Easily design NFTs on IC network and sell them on Shopify just like regular products. It's so much fun to send NFTs to your customers' emails! Discover how IC NFT Creator can power up your entrepreneurial journey."
+          popoverActions={[{ content: "Dismiss", onAction: () => {} }]}
+          size="small"
+        >
+          <img
+            alt=""
+            width="100%"
+            height="100%"
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+            src={logo}
+          />
+        </MediaCard>
+        <Grid>
+          <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+            <LegacyCard title="Collection" sectioned>
+              <p>View a summary of your online store’s sales.</p>
+            </LegacyCard>
+          </Grid.Cell>
+          <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+            <LegacyCard title="NFT" sectioned>
+              <p>View a summary of your online store’s orders.</p>
+            </LegacyCard>
+          </Grid.Cell>
+        </Grid>
+      </BlockStack>
     </Page>
   );
 }
